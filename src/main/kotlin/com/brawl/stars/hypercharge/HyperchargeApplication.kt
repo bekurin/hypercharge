106af0924e9a37fb8c254dcdf32120c43ba387c5
chungchung <hangjastar@gmail.com>
2025-12-29 22:25:05 +0900

    feat: init project

---
+++ src/main/kotlin/com/brawl/stars/hypercharge/HyperchargeApplication.kt
@@ -0,0 +1,11 @@
+package com.brawl.stars.hypercharge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HyperchargeApplication
+
+fun main(args: Array<String>) {
+	runApplication<HyperchargeApplication>(*args)
+}
---
+++ src/test/kotlin/com/brawl/stars/hypercharge/HyperchargeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.brawl.stars.hypercharge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HyperchargeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
